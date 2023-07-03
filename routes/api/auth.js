a583adfa2b82a1a9ee4e5fca22c12069b56f76b4
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-07-03 17:43:20 +0300

    lessons_1

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {validationBody, authenticate, isValidId} = require("../../middlewares")
+const {validationBody, authenticate, isValidId, upload} = require("../../middlewares")
 const {schemas} = require("../../models/user")
 const ctrl = require("../../controllers/auth")
 
@@ -13,8 +13,17 @@ router.post("/login", validationBody(schemas.loginSchema), ctrl.login)
 
 router.get("/current", authenticate, ctrl.getCurrent)
 
-router.post("/logout", authenticate, ctrl.logout)
+// router.post("/avatars", authenticate, ctrl.logout)
 
 router.patch("/:id/subscription", authenticate, isValidId,  validationBody(schemas.subscriptionSchema), ctrl.updateSubscription);
 
+
+// для зміни аватара. поле Avatar
+// upload.fields([{name:"cover", maxCount:1},{name:"subcover",1}]) масив обєктів
+// upload.array("avatar", 8)
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
+// router.patch("/avatars",  upload.single("avatar"), async(req, res) =>{
+//    console.log('req.body', req.body)
+//    console.log('req.body', req.file)
+// })
 module.exports = router
